406ff1f9e68cd6e05c722280d153302c609d4ef4
Robert Winkler <rwinkler@antmicro.com>
2019-10-15 16:37:59 +0200

    Add Symbolator for generating symbols of verilog models
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,10 @@ needs_sphinx = '1.6'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo']
+extensions = [
+    'sphinx.ext.todo',
+    'symbolator_sphinx',
+]
 
 numfig = True
 
@@ -289,3 +292,9 @@ rst_prolog = """
 .. role:: raw-html(raw)
    :format: html
 """
+
+### SYMBOLATOR ###
+
+symbolator_cmd_args       = ['--transparent']
+symbolator_output_format  = 'svg'
+
